2d449085ddbe7fc951eb7e1d6f8d51353ce0d93d
Van Ni <vanni4018@gmail.com>
2024-05-25 21:57:42 +0700

    fea: Sequelize Migration & Seeds (test)

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -6,9 +6,5 @@ export const env = {
     APP_HOST: process.env.APP_HOST,
     APP_PORT: process.env.APP_PORT,
 
-    MONGODB_URL: process.env.MONGODB_URL,
-    DATABASE_NAME: process.env.DATABASE_NAME,
-    AUTHOR: process.env.AUTHOR,
-
     BUILD_MODE: process.env.BUILD_MODE
 }
