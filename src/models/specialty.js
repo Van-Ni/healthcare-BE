d2050ccf17ec4b446edd060fa69617770d4026e0
Van Ni <vanni4018@gmail.com>
2024-05-27 20:00:09 +0700

    feat: add specialty, doctor-clinic-specialty models

--- src/models/specialty.js
+++ src/models/specialty.js
@@ -1,10 +1,9 @@
 'use strict';
 const {
   Model
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Specialty extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -14,17 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    address: DataTypes.STRING,
-    gender: DataTypes.BOOLEAN,
-    roleId: DataTypes.INTEGER,
+  Specialty.init({
+    description: DataTypes.TEXT,
+    image: DataTypes.STRING,
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Specialty',
   });
-  return User;
+  return Specialty;
 };
